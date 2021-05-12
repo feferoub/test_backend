04c0a5b338003070b0510c224f7e1bf857238bae
felix roubaud <fel.roub@gmail.com>
2021-05-12 19:53:30 +0000

    Test cloud debugger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ require('@google-cloud/debug-agent').start({
   serviceContext: { enableCanary: true },
 });
 
+// test
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
